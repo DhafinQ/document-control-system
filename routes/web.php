a2f0f6c50bb05173d0ba3b49cbd56697cfd41dfd
Evnxtt <Umaru.himotou@gmail.com>
2025-01-12 14:49:55 +0700

    Template Frontend

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/template', function () {
+    return view('/template/home');
+});
+
+Route::get('/template/buttons', function () {
+    return view('/template/buttons');
+});
+
+Route::get('/template/alerts', function () {
+    return view('/template/alerts');
+});
+
+Route::get('/template/card', function () {
+    return view('/template/card');
+});
+
+Route::get('/template/forms', function () {
+    return view('/template/forms');
+});
+
+Route::get('/template/typography', function () {
+    return view('/template/typography');
+});
+
+Route::get('/template/login', function () {
+    return view('/template/login');
+});
+
+Route::get('/template/register', function () {
+    return view('/template/register');
+});
+
+Route::get('/template/icon', function () {
+    return view('/template/icon');
+});
+
+Route::get('/template/sample', function () {
+    return view('/template/sample');
+});
