eb86de77cdff155d719fb258e8b6ce7934459633
DhafinQ <dhafinqintarakh@gmail.com>
2025-01-14 14:56:27 +0700

    Remove Role Migration&Model (Conflict RBAC), Fix Migration Rollback

--- database/migrations/2025_01_10_135606_alter_document_table_add_revision.php
+++ database/migrations/2025_01_10_135606_alter_document_table_add_revision.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('documents', function (Blueprint $table) {
-            $table->dropColumn('current_revision_id');
+            $table->dropForeign(['current_revision_id']);
         });
     }
 };
