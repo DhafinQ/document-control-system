6f58a5bb41b3dec6cceb3601f5a2c64475e17015
DhafinQ <dhafinqintarakh@gmail.com>
2025-01-17 21:03:37 +0700

    Fix Config

--- config/rbac.php
+++ config/rbac.php
@@ -6,8 +6,6 @@ return [
     'adminUserId' => null, // You must set it. This is the initial user id, which must be an administrator, at least at the first stage.
     'routesMainPermission' => Itstructure\LaRbac\Models\Permission::ADMINISTRATE_PERMISSION,  // You can change it
     'routesAuthMiddlewares' => ['auth'],  // You can change it
-    'memberNameAttributeKey' => function ($row) { // You can change it. And can simply set 'memberNameAttributeKey' => 'name'
-        return $row->name;
-    },
+    'memberNameAttributeKey' => 'name',
     'rowsPerPage' => 10,
 ];
