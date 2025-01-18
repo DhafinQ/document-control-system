b767c5af44c969d43c1a5b404cac3db79649db01
RadenRizky26 <radenr775@gmail.com>
2025-01-18 16:22:57 +0700

    Manager daftar + edit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,38 @@
+    </div>
+  <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+  <script src="{{ asset('assets/js/app.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+  <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+  
+</body>
+
+</html></div>
+  <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+  <script src="{{ asset('assets/js/app.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+  <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <!-- Script Datatable -->
+  <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+
+  <!-- Inisialisasi DataTable -->
+  <script>
+      $(document).ready(function () {
+          $('#myTable').DataTable({
+              "paging": true,     
+              "searching": true,  
+              "ordering": true,   
+          });
+      });
+  </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,65 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/welcome');
+});
+
+Route::get('/template', function () {
+    return view('/template/home');
+});
+
+Route::get('/template/buttons', function () {
+    return view('/template/buttons');
+});
+
+Route::get('/template/alerts', function () {
+    return view('/template/alerts');
+});
+
+Route::get('/template/card', function () {
+    return view('/template/card');
+});
+
+Route::get('/template/forms', function () {
+    return view('/template/forms');
+});
+
+Route::get('/template/typography', function () {
+    return view('/template/typography');
+});
+
+Route::get('/template/login', function () {
+    return view('/template/login');
+});
+
+Route::get('/template/register', function () {
+    return view('/template/register');
+});
+
+Route::get('/template/icon', function () {
+    return view('/template/icon');
+});
+
+Route::get('/template/sample', function () {
+    return view('/template/sample');
+});
+
+Route::get('/admin/tambah', function () {
+    return view('/admin/tambah');
+});
+
+Route::get('/admin/daftar', function () {
+    return view('/admin/daftar');
+});
+
+Route::get('/admin/edit', function () {
+    return view('/admin/edit');
+});
+
+Route::get('/manager/tambah', function () {
+    return view('/manager/tambah');
+});
+
+Route::get('/manager/daftar', function () {
+    return view('/manager/daftar');
 });
