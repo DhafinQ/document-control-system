6ca2293bd1bfed8ec68c63f3dc3af4c160b868a2
Arthuriant <fundito079@gmail.com>
2025-01-22 07:29:28 +0700

    Update warna dan fitur
    - Sesuai request ici bos

--- resources/views/admin/approval_dokumen.blade.php
+++ resources/views/admin/approval_dokumen.blade.php
@@ -12,7 +12,7 @@
 
 
   
-                <!-- Modal Tolak-->
+                <!-- Modal Revisi-->
                 <div class="modal fade" id="modalTolak" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                     <div class="modal-content">
@@ -54,21 +54,7 @@
                                     <label for="exampleInputEmail1" class="form-label">Pengunggah</label>
                                     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                                   </div>
-                                  <div class="col-md-6">
-                                    <label for="exampleInputEmail1" class="form-label">Status</label>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Telah diverivikasi Pengendali dokumen
-                                        </label>
-                                      </div>
-                                      <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                                        <label class="form-check-label" for="flexCheckChecked">
-                                          Telah diverifikasi bagian mutu
-                                        </label>
-                                      </div>
-                                  </div>
+                                
                                 </div>
                                   <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label">Alasan Penolakan</label>
@@ -78,7 +64,7 @@
                         </div>
                         <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                        <button type="button" class="btn btn-primary">Konfirmasi Penolakan</button>
+                        <button type="button" class="btn btn-danger">Konfirmasi Penolakan</button>
                         </div>
                     </div>
                     </div>
@@ -146,7 +132,7 @@
                             </div>
                             <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                            <button type="button" class="btn btn-primary">Konfirmasi Pengesahan</button>
+                            <button type="button" class="btn btn-success">Konfirmasi Pengesahan</button>
                             </div>
                         </div>
                         </div>
@@ -178,8 +164,8 @@
                                         <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalTerima">
                                             Terima
                                         </button>
-                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalTolak">
-                                            Tolak
+                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modalTolak">
+                                            Revisi
                                         </button>
                                     </td>
 
@@ -195,9 +181,9 @@
                                         <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalTerima">
                                             Terima
                                         </button>
-                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalTolak">
-                                            Tolak
-                                        </button>
+                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modalTolak">
+                                          Revisi
+                                      </button>
                                     </td>
                                 </tr>
                                 <tr>
@@ -211,9 +197,9 @@
                                         <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalTerima">
                                             Terima
                                         </button>
-                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalTolak">
-                                            Tolak
-                                        </button>
+                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modalTolak">
+                                          Revisi
+                                      </button>
                                     </td>
 
                                 </tr>
