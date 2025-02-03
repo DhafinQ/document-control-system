4e301e821921c18f824f864a91d3fdfe4542ad92
Arthuriant <fundito079@gmail.com>
2025-02-03 15:47:26 +0700

    Radio Button
    - Add radio button for datatable approval
    - Add radio button for data table document
    
    Notes: masukan fungsi backendnya di JS pada footer, perhatikan comment "//masukin fungsi untuk setiap radio disini"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,57 +1,137 @@
-    </div>
-    
-    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    {{-- <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script> --}}
-    <script src="{{ asset('assets/js/app.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
-    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="jQuery.js"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/sol-2.0.0.js') }}"></script>
+<!-- jQuery & Bootstrap -->
+<script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 
-    <!-- Script Datatable -->
-    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+<!-- Additional Libraries -->
+<script src="{{ asset('assets/js/app.min.js') }}"></script>
+<script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+<script src="{{ asset('assets/js/dashboard.js') }}"></script>
 
-    <link rel="stylesheet"  href="{{ asset('assets/css/searchableOptionList.css') }}">
-    <script type="text/javascript" src="jQuery.js"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/sol-2.0.0.js') }}"></script>
+<!-- jQuery (CDN & Local) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="jQuery.js"></script>
 
-    <script type="text/javascript">
-        $(function() {
-            // initialize sol
-            $('#my-select').searchableOptionList();
-       });
-    </script>
+<!-- Searchable Option List (SOL) -->
+<script src="{{ asset('assets/js/sol-2.0.0.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('assets/css/searchableOptionList.css') }}">
 
-    <!-- Inisialisasi DataTable -->
-    <script>
+<!-- DataTables -->
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
 
-    var url = window.location.pathname;
-    var orderConfig;
+<!-- Dropdown Notification -->
+<script src="{{ asset('assets/js/dropdownNotification.js') }}"></script>
 
-    if (url === '/notifications') {
-        orderConfig = [[2, "desc"]];
-    } else if (url === '/categories') {
-        orderConfig = [[0, "asc"]];
-    } else {
-        orderConfig = [[5, "desc"]];
-    }
+<!-- Inisialisasi Searchable Option List -->
+<script>
+    $(function () {
+        $('#my-select').searchableOptionList();
+    });
+</script>
 
-        $(document).ready(function () {
-            $('#myTable').DataTable({
-                "paging": true,     
-                "searching": true,  
-                "ordering": true,
-                "order": orderConfig  
-            });
+<!-- Inisialisasi DataTable -->
+<script>
+    $(document).ready(function () {
+        var url = window.location.pathname;
+        var orderConfig;
+
+        if (url === '/notifications') {
+            orderConfig = [[2, "desc"]];
+        } else if (url === '/categories') {
+            orderConfig = [[0, "asc"]];
+        } else {
+            orderConfig = [[5, "desc"]];
+        }
+
+        // DataTable Biasa
+        $('#myTable').DataTable({
+            "paging": true,
+            "searching": true,
+            "ordering": true,
         });
-        </script>
-    </body>
 
-    <script src="{{asset('assets/js/dropdownNotification.js')}}"></script>
-    @yield('customJS')
+        // DataTable Approval
+        let tableApproval = $('#tableApproval').DataTable({
+            "paging": true,
+            "searching": true,
+            "ordering": true,
+            "dom": "<'d-flex justify-content-between'lf>rtip",
+        });
 
+        let searchBoxApproval = $('#tableApproval_wrapper .dataTables_filter');
+        searchBoxApproval.addClass('d-flex align-items-center');
+
+        let radioButtonsApproval = `
+            <div class="me-3">
+                <label class="me-1"><input type="radio" name="filterApproval" value="all" checked> Semua</label>
+                <label class="me-1"><input type="radio" name="filterApproval" value="approved"> Approved</label>
+                <label class="me-1"><input type="radio" name="filterApproval" value="pending"> Pending</label>
+            </div>
+        `;
+
+        searchBoxApproval.prepend(radioButtonsApproval);
+
+        $('input[name="filterApproval"]').on('change', function () {
+            let filterValue = $('input[name="filterApproval"]:checked').val();
+            let labelText = "Menampilkan: Semua";
+
+            //masukin fungsi untuk setiap radio disini
+            if (filterValue === "all") {
+                tableApproval.column(3).search("").draw();
+                labelText = "Menampilkan: Semua";
+            } else if (filterValue === "approved") {
+                tableApproval.column(3).search("approved", true, false).draw();
+                labelText = "Menampilkan: Disetujui";
+            } else if (filterValue === "pending") {
+                tableApproval.column(3).search("pending", true, false).draw();
+                labelText = "Menampilkan: Pending";
+            }
+
+            $("#dynamicLabel").html(labelText);
+        });
+
+        // DataTable Document
+        let tableDocument = $('#tableDocument').DataTable({
+            "paging": true,
+            "searching": true,
+            "ordering": true,
+            "dom": "<'d-flex justify-content-between'lf>rtip",
+        });
+
+        let searchBoxDocument = $('#tableDocument_wrapper .dataTables_filter');
+        searchBoxDocument.addClass('d-flex align-items-center');
+
+        let radioButtonsDocument = `
+            <div class="me-3">
+                <label class="me-1"><input type="radio" name="filterDocument" value="all" checked> Semua</label>
+                <label class="me-1"><input type="radio" name="filterDocument" value="aktif"> Aktif</label>
+                <label class="me-1"><input type="radio" name="filterDocument" value="kadaluarsa"> Kadaluarsa</label>
+            </div>
+        `;
+
+        searchBoxDocument.prepend(radioButtonsDocument);
+
+        $('input[name="filterDocument"]').on('change', function () {
+            let filterValue = $('input[name="filterDocument"]:checked').val();
+            let labelText = "Menampilkan: Semua";
+            //masukin fungsi untuk setiap radio disini
+            if (filterValue === "all") {
+                tableDocument.column(3).search("").draw();
+                labelText = "Menampilkan: Semua";
+            } else if (filterValue === "aktif") {
+                tableDocument.column(3).search("approved", true, false).draw();
+                labelText = "Menampilkan: Aktif";
+            } else if (filterValue === "kadaluarsa") {
+                tableDocument.column(3).search("pending", true, false).draw();
+                labelText = "Menampilkan: Kadaluarsa";
+            }
+
+            $("#dynamicLabel2").html(labelText);
+        });
+    });
+</script>
+
+@yield('customJS')
+</body>
 </html>
