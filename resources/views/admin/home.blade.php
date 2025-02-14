08db85e1a20e2147acce994fb0489cb03801eac1
Arthuriant <fundito079@gmail.com>
2025-02-14 21:15:00 +0700

    datatable dashboard
    sss

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -100,6 +100,97 @@
               </div>
             </div>
           </div>
+
+          <div class="table-responsive mt-4">
+            <table id="tableDocument" class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Judul</th>
+                        <th>Kategori</th>
+                        <th>Status</th>
+                        <th>Uploader</th>
+                        <th>Created At</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Tutorial Laravel</td>
+                      <td>Programming</td>
+                      <td>Published</td>
+                      <td>Andika</td>
+                      <td>2025-02-14</td>
+                      <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Introduction to AR</td>
+                      <td>Technology</td>
+                      <td>Draft</td>
+                      <td>Andika</td>
+                      <td>2025-02-13</td>
+                      <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Unity Game Development</td>
+                      <td>Game Development</td>
+                      <td>Published</td>
+                      <td>Andika</td>
+                      <td>2025-02-12</td>
+                      <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                      </td>
+                    </tr>                  
+                    {{-- @foreach ($documents as $document)
+                    <tr>
+                        <td>{{$document->code}}</td>
+                        <td>{{$document->title}}</td>
+                        <td>{{$document->category->name}}</td>
+                        <td>
+                            @php
+                                $currentStatus = ($document->currentRevision->document_id === $document->id) ? $document->latestHistory->revision->status : 'Expired'
+                            @endphp
+                            <span class="badge
+                            @if ($currentStatus === 'Draft')
+                                bg-light text-dark
+                            @elseif ($currentStatus === 'Disetujui')
+                                bg-success
+                            @elseif ($currentStatus === 'Pengajuan Revisi' || $currentStatus === 'Proses Revisi')
+                                bg-warning
+                            @elseif ($currentStatus === 'Expired')
+                                bg-danger
+                            @endif
+                            ">
+                                {{ $document->latestHistory->revision->status }}
+                            </span>
+                        </td>
+                        <td>{{$document->uploader->name}}</td>
+                        <td>{{ \Carbon\Carbon::parse($document->created_at)->format('d/m/Y-H:i:s') }}</td>
+                        <td>
+                            @canany(['edit-documents','edit-revisions'])
+                            <div class="d-flex">
+                                <a href="{{ route('document_revision.show',['documentRevision' => $document->latestHistory->revision->id]) }}" class="btn btn-sm btn-admin me-1">Detail</a>
+                                @if ($document->currentRevision->document_id === $document->id && ($document->latestHistory->revision->status == 'Disetujui' || $document->latestHistory->revision->status == 'Pengajuan Revisi'))
+                                <a href="{{ route('document_revision.edit', $document->latestHistory->revision->id) }}" class="btn btn-sm btn-approver">Revisi</a>
+                                @endif
+                            </div>
+                            @endcanany
+                        </td>
+                    </tr>
+                    @endforeach --}}
+                </tbody>
+            </table>
+          </div>
               </div>
             </div>
           </div>
@@ -107,5 +198,7 @@
       </div>
     </div>
 
-    
+    @section('customJS')
+    <script src="{{asset('assets/js/datatablesDocuments.js')}}"></script>
+    @endsection
 @endsection
